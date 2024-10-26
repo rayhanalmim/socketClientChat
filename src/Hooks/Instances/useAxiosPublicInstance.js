3f1338ef8e7d9b816e7467d7ab70457660442a71
taijulsir <princetaijul123@gmail.com>
2024-10-26 22:16:53 +0600

    fixed auth apis

--- src/Hooks/Instances/useAxiosPublicInstance.js
+++ src/Hooks/Instances/useAxiosPublicInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosAuthInstance = axios.create({
-    baseURL: import.meta.env.VITE_APP_BACKEND_URL + 'api/moderatorApp/public/',
+    baseURL: import.meta.env.VITE_APP_BACKEND_URL + 'api/employeeApp/public/',
 })
